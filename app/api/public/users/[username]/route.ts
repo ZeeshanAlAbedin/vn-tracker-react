bfcee21c34ab2d1d0abe80dfa0bbb0e81c65c808
= <zeeshanalabedin@gmail.com>
2026-01-11 15:19:56 +0530

    remove routes.js from users for now

--- app/api/public/users/[username]/route.ts
+++
@@ -1,6 +0,0 @@
-// Need POST for creating user on first login and setup user_settings
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function POST(request: NextRequest, { params }: { params: { username: string } }) {
-  
-} 
